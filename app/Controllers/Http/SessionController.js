c605502a244a5e0265f66f0cfdf8f8e8c04126e8
Jovanir Mateus de Almeida Lopes <jovanir.mateus@gmail.com>
2021-02-06 08:32:17 -0300

    Adicionado funcao para checar token

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -10,6 +10,15 @@ class SessionController {
 
     return token
   }
+
+  async checkToken({auth, response}) {
+    try {
+      await auth.check()
+      response.status(200).send('logado')
+    } catch (error) {
+      response.status(401).send('Token invalido')
+    }
+  }
 }
 
 module.exports = SessionController
