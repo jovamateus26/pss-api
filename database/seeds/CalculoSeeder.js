db29ac671ca568d899ac70f3e4ce4cd6e33cb09c
Jovanir Mateus de Almeida Lopes <jovamateus26@gmail.com>
2021-02-09 09:10:02 -0300

    Criado a migração com os dados para calculo

--- database/seeds/CalculoSeeder.js
+++ database/seeds/CalculoSeeder.js
@@ -51,6 +51,146 @@ class CalculoSeeder {
       "titulo": "experiencia",
       "descricao": "Experiência na docência de Professor de   Educação Infantil de 2011 a 2020."
     })
+    await CalculoModel.create({
+      "vaga_id": "2",
+      "pontuacao": "15",
+      "pontuacaoMax": "15",
+      "titulo": "magisterio",
+      "descricao": "Curso de nível médio completo na modalidade normal (MAGISTÉRIO)."
+    })
+    await CalculoModel.create({
+      "vaga_id": "2",
+      "pontuacao": "35",
+      "pontuacaoMax": "35",
+      "titulo": "licenciaturaPlenaPedagogia",
+      "descricao": "Licenciatura plena completa em PEDAGOGIA com habilitação para o magistério ou CURSO NORMAL SUPERIOR reconhecido pelo Mec."
+    })
+    await CalculoModel.create({
+      "vaga_id": "2",
+      "pontuacao": "20",
+      "pontuacaoMax": "20",
+      "titulo": "licenciaturaPlena",
+      "descricao": "Licenciatura Plena completa numa das áreas do conhecimento da Educação Básica,  diferente do curso apresentado para contratação."
+    })
+    await CalculoModel.create({
+      "vaga_id": "2",
+      "pontuacao": "10",
+      "pontuacaoMax": "20",
+      "titulo": "posGraduacao",
+      "descricao": "Pós-Graduação completa em nível de especialização;"
+    })
+    await CalculoModel.create({
+      "vaga_id": "2",
+      "pontuacao": "1",
+      "pontuacaoMax": "10",
+      "titulo": "experiencia",
+      "descricao": "Experiência na docência de Professor de   Educação Infantil de 2011 a 2020."
+    })
+    await CalculoModel.create({
+      "vaga_id": "3",
+      "pontuacao": "15",
+      "pontuacaoMax": "15",
+      "titulo": "magisterio",
+      "descricao": "Curso de nível médio completo na modalidade normal (MAGISTÉRIO)."
+    })
+    await CalculoModel.create({
+      "vaga_id": "3",
+      "pontuacao": "35",
+      "pontuacaoMax": "35",
+      "titulo": "licenciaturaPlenaPedagogia",
+      "descricao": "Licenciatura plena completa em PEDAGOGIA com habilitação para o magistério ou CURSO NORMAL SUPERIOR reconhecido pelo Mec."
+    })
+    await CalculoModel.create({
+      "vaga_id": "3",
+      "pontuacao": "20",
+      "pontuacaoMax": "20",
+      "titulo": "licenciaturaPlena",
+      "descricao": "Licenciatura Plena completa numa das áreas do conhecimento da Educação Básica,  diferente do curso apresentado para contratação."
+    })
+    await CalculoModel.create({
+      "vaga_id": "3",
+      "pontuacao": "10",
+      "pontuacaoMax": "20",
+      "titulo": "posGraduacao",
+      "descricao": "Pós-Graduação completa em nível de especialização;"
+    })
+    await CalculoModel.create({
+      "vaga_id": "3",
+      "pontuacao": "1",
+      "pontuacaoMax": "10",
+      "titulo": "experiencia",
+      "descricao": "Experiência na docência de Professor de   Educação Infantil de 2011 a 2020."
+    })
+    await CalculoModel.create({
+      "vaga_id": "4",
+      "pontuacao": "15",
+      "pontuacaoMax": "15",
+      "titulo": "magisterio",
+      "descricao": "Curso de nível médio completo na modalidade normal (MAGISTÉRIO)."
+    })
+    await CalculoModel.create({
+      "vaga_id": "4",
+      "pontuacao": "35",
+      "pontuacaoMax": "35",
+      "titulo": "licenciaturaPlenaPedagogia",
+      "descricao": "Licenciatura plena completa em PEDAGOGIA com habilitação para o magistério ou CURSO NORMAL SUPERIOR reconhecido pelo Mec."
+    })
+    await CalculoModel.create({
+      "vaga_id": "4",
+      "pontuacao": "20",
+      "pontuacaoMax": "20",
+      "titulo": "licenciaturaPlena",
+      "descricao": "Licenciatura Plena completa numa das áreas do conhecimento da Educação Básica,  diferente do curso apresentado para contratação."
+    })
+    await CalculoModel.create({
+      "vaga_id": "4",
+      "pontuacao": "10",
+      "pontuacaoMax": "20",
+      "titulo": "posGraduacao",
+      "descricao": "Pós-Graduação completa em nível de especialização;"
+    })
+    await CalculoModel.create({
+      "vaga_id": "4",
+      "pontuacao": "1",
+      "pontuacaoMax": "10",
+      "titulo": "experiencia",
+      "descricao": "Experiência na docência de Professor de   Educação Infantil de 2011 a 2020."
+    })
+    await CalculoModel.create({
+      "vaga_id": "5",
+      "pontuacao": "15",
+      "pontuacaoMax": "15",
+      "titulo": "magisterio",
+      "descricao": "Curso de nível médio completo na modalidade normal (MAGISTÉRIO)."
+    })
+    await CalculoModel.create({
+      "vaga_id": "5",
+      "pontuacao": "35",
+      "pontuacaoMax": "35",
+      "titulo": "licenciaturaPlenaPedagogia",
+      "descricao": "Licenciatura plena completa em PEDAGOGIA com habilitação para o magistério ou CURSO NORMAL SUPERIOR reconhecido pelo Mec."
+    })
+    await CalculoModel.create({
+      "vaga_id": "5",
+      "pontuacao": "20",
+      "pontuacaoMax": "20",
+      "titulo": "licenciaturaPlena",
+      "descricao": "Licenciatura Plena completa numa das áreas do conhecimento da Educação Básica,  diferente do curso apresentado para contratação."
+    })
+    await CalculoModel.create({
+      "vaga_id": "5",
+      "pontuacao": "10",
+      "pontuacaoMax": "20",
+      "titulo": "posGraduacao",
+      "descricao": "Pós-Graduação completa em nível de especialização;"
+    })
+    await CalculoModel.create({
+      "vaga_id": "5",
+      "pontuacao": "1",
+      "pontuacaoMax": "10",
+      "titulo": "experiencia",
+      "descricao": "Experiência na docência de Professor de   Educação Infantil de 2011 a 2020."
+    })
   }
 }
 
